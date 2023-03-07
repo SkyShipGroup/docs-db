889059f491341917159d90aebc19010cc076a2f5
SkyShipGroup <126949734+SkyShipGroup@users.noreply.github.com>
2023-03-07 17:32:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,134 @@
 
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <meta charset="UTF-8">
+
+    <title>Мой сайт</title>
+
+    <style>
+
+      * {
+
+        margin: 0;
+
+        padding: 0;
+
+        box-sizing: border-box;
+
+      }
+
+      body {
+
+        font-family: Arial, sans-serif;
+
+      }
+
+      /* Стили для бокового меню */
+
+      .sidebar {
+
+        position: fixed; /* фиксированное положение */
+
+        top: 0;
+
+        left: 0;
+
+        height: 100%;
+
+        width: 200px;
+
+        background-color: #333;
+
+        color: #fff;
+
+        padding: 20px;
+
+      }
+
+      .sidebar ul {
+
+        list-style: none;
+
+        margin: 0;
+
+        padding: 0;
+
+      }
+
+      .sidebar li {
+
+        margin-bottom: 10px;
+
+      }
+
+      .sidebar a {
+
+        color: #fff;
+
+        text-decoration: none;
+
+      }
+
+      /* Стили для основного содержимого */
+
+      .content {
+
+        margin-left: 200px; /* ширина бокового меню */
+
+        padding: 20px;
+
+      }
+
+      .content h1 {
+
+        font-size: 36px;
+
+        margin-bottom: 20px;
+
+      }
+
+      .content p {
+
+        font-size: 16px;
+
+        line-height: 1.5;
+
+        margin-bottom: 20px;
+
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+
+    <div class="sidebar">
+
+      <ul>
+
+        <li><a href="#">Главная</a></li>
+
+        <li><a href="#">О нас</a></li>
+
+        <li><a href="#">Контакты</a></li>
+
+      </ul>
+
+    </div>
+
+    <div class="content">
+
+      <h1>Заголовок страницы</h1>
+
+      <p>Текст страницы...</p>
+
+    </div>
+
+  </body>
+
+</html>
